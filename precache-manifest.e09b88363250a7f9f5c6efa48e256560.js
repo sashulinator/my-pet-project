66366a648007e06aba8dbdb3e0fec692c2e31a34
savchenko alexander <ext.adsavchenko@tinkoff.ru>
2021-09-13 00:02:02 +0400

    Updates

---
+++ precache-manifest.e09b88363250a7f9f5c6efa48e256560.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb5a8886284056428e1ecc43b62be30",
+    "url": "/my-pet-project/404.html"
+  },
+  {
+    "revision": "f7581111ff4064c87233",
+    "url": "/my-pet-project/static/css/2.2bc212ea.chunk.css"
+  },
+  {
+    "revision": "340eb0b80bfde589ffa8",
+    "url": "/my-pet-project/static/css/main.58993543.chunk.css"
+  },
+  {
+    "revision": "f7581111ff4064c87233",
+    "url": "/my-pet-project/static/js/2.bc4df64f.chunk.js"
+  },
+  {
+    "revision": "6e3c19c1529f94fac969084491e122c6",
+    "url": "/my-pet-project/static/js/2.bc4df64f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "340eb0b80bfde589ffa8",
+    "url": "/my-pet-project/static/js/main.90222036.chunk.js"
+  },
+  {
+    "revision": "28d7369d3339975a24dc",
+    "url": "/my-pet-project/static/js/runtime-main.ecc5a8aa.js"
+  },
+  {
+    "revision": "3de15e25c732a0bb99ce186d5a76d54c",
+    "url": "/my-pet-project/static/media/minibus.3de15e25.jpg"
+  }
+]);
